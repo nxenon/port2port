d4fa9d978b784722ba31a1a65c2e03c88697588b
nxenon <Khodexenon@gmail.com>
2021-02-17 17:47:28 +0330

    add http service

--- port2port.py
+++ port2port.py
@@ -1,15 +1,17 @@
 import socket
 from argparse import ArgumentParser
 from threading import Thread
-from time import sleep
 
 class Port2Port:
 
-    def __init__(self ,listen_port ,remote_port ,listen_ip ,remote_ip):
+    def __init__(self ,listen_port ,remote_port ,listen_ip ,remote_ip ,service):
         self.listen_port = int(listen_port)
         self.remote_port = int(remote_port)
         self.listen_ip = listen_ip
         self.remote_ip = remote_ip
+        self.service = service
+        if self.service :
+            self.service = self.service.lower()
         self.is_socket_closed = True
 
     def start(self):
@@ -67,26 +69,45 @@ class Port2Port:
             Thread(target=self.forward_from_remote_port).start()
 
     def forward_to_remote_port(self):
+        '''
+        this function forwards client socket data to remote socket
+        '''
+
         try :
             while True :
                 client_data = self.client_conn.recv(4096)
                 if not client_data :
                     self.close_both_connection()
                     break
-                self.remote_socket.sendall(client_data)
+                try:
+                    final_data = (self.commit_client_service_filters(data=client_data.decode() ,_from='client')).encode()
+                except:
+                    final_data = client_data
+
+                self.remote_socket.sendall(final_data)
 
         except Exception as e:
             print(f'Error --> {e}')
             self.close_both_connection()
 
     def forward_from_remote_port(self):
+        '''
+        this function forwards remote socket data to client socket
+        '''
+
         try :
             while True :
                 remote_data = self.remote_socket.recv(4096)
                 if not remote_data :
                     self.close_both_connection()
                     break
-                self.client_conn.sendall(remote_data)
+                try :
+                    final_data = (self.commit_client_service_filters(data=remote_data.decode() ,_from='remote')).encode()
+                except :
+                    final_data = remote_data
+
+                self.client_conn.sendall(final_data)
+
 
         except Exception as e:
             print(f'Error --> {e}')
@@ -102,6 +123,37 @@ class Port2Port:
         except Exception as e:
             print(f'Error --> {e}')
 
+    def commit_client_service_filters(self ,data ,_from):
+        '''
+        this function process data and change them if it is necessary and if service is not None
+        :param self:
+        :param :data
+        data is data from/to remote port before processing
+        :param _from:
+        _from can be "remote" ---> from remote socket  or "client" ---> from client socket
+        :return:
+        '''
+        sc_name = self.service # sc_name is service name like http
+        processed_data = None # return var
+
+        if sc_name is None :
+            processed_data = data
+            return processed_data
+
+        if sc_name == 'http' :
+            if _from == 'client' :
+                data = data.replace(self.client_conn.getsockname()[0] ,self.remote_ip)
+                data = data.replace(f':{self.listen_port}' ,f':{self.remote_port}')
+                processed_data = data
+                return processed_data
+
+            elif _from == 'remote' :
+                data = data.replace(self.remote_ip ,self.client_conn.getsockname()[0])
+                data = data.replace(f':{self.remote_port}' ,f':{self.listen_port}')
+                processed_data = data
+                return processed_data
+
+
 def print_help():
     help_msg = '''
 
@@ -114,6 +166,7 @@ arguments:
 optional arguments :
    --listen-ip or -lip LISTEN_IP (ip address to listen [default : 0.0.0.0])
    --remote-ip or -rip LISTEN_IP (ip address to forward [default : 127.0.0.1])
+   --service or -sc SERVICE (Determine Service : [http])
    
    -h or --help                  (show this help message and exit)
     '''
@@ -126,10 +179,11 @@ if __name__ == '__main__':
     parser.add_argument('-rp' ,'--remote-port' ,help='remote port to forward')
     parser.add_argument('-lip' ,'--listen-ip' ,help='ip address to listen [default : 0.0.0.0]' ,default='0.0.0.0')
     parser.add_argument('-rip', '--remote-ip', help='ip address to forward [default : 127.0.0.1]', default='127.0.0.1')
+    parser.add_argument('-sc' ,'--service' ,help='Determine Service [http]' ,default=None)
     args ,unknown = parser.parse_known_args()
 
     if (args.listen_port) and (args.remote_port):
-        port2port = Port2Port(args.listen_port ,args.remote_port ,args.listen_ip ,args.remote_ip)
+        port2port = Port2Port(args.listen_port ,args.remote_port ,args.listen_ip ,args.remote_ip ,args.service)
         port2port.start()
     else:
         print_help()
